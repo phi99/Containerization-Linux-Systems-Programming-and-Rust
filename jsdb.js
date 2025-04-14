6c14f5201a4a4c369d67c435ddc48b590b25e432
phi99 <34248917+phi99@users.noreply.github.com>
2025-04-15 02:43:41 +1000

    Create jsdb.js

---
+++ jsdb.js
@@ -0,0 +1,20 @@
+const {Client} = require('pg')
+
+const client = new Client({
+	host: 
+	user: 
+	port: 
+	password:
+	database: 
+})
+
+client.connect();
+
+client.query('Select * from test_table1', (err,res)=>{
+	if(err){
+		console.log(res.rows);
+	} else {
+		console.log(err.message);
+	}
+	client.end;
+})
